89660448efc89284373d5acb49eb60b2831583d6
andriangungon <andriangungon@Andrians-Macbook-Pro-Retina.local>
2014-02-20 01:14:30 +0800

    iOS Development Practice
    My first native iOS development

---
+++ CrystalBall/CrystalBall/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CrystalBall
+//
+//  Created by Andrian Gungon on 2/20/14.
+//  Copyright (c) 2014 Andrian Gungon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ CrystalBall/CrystalBall/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CrystalBall
+//
+//  Created by Andrian Gungon on 2/20/14.
+//  Copyright (c) 2014 Andrian Gungon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
